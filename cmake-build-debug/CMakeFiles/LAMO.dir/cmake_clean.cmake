eb1e2d58e1414df17b775fcee88e74ec65abb72d
Samuel <samuel.ahou@student.uclouvain.be>
2021-07-23 20:23:14 +0200

    Inital Commit

---
+++ cmake-build-debug/CMakeFiles/LAMO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAMO.dir/Functions/LAF.c.o"
+  "CMakeFiles/LAMO.dir/Functions/UF.c.o"
+  "CMakeFiles/LAMO.dir/main.c.o"
+  "LAMO"
+  "LAMO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LAMO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
